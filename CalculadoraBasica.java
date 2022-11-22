321e92b5416a241374ce8a5be4f9c4aec86554d4
juanma785 <115530852+juanma785@users.noreply.github.com>
2022-11-22 18:19:49 +0100

    Add files via upload

---
+++ CalculadoraBasica.java
@@ -0,0 +1,15 @@
+public class CalculadoraBasica{
+    Integer suma (Integer a, Integer b){
+        return a +b;
+    }
+    Integer  resta (Integer a, Integer b){
+return a - b;
+    }
+    Integer  multiplicar (Integer a, Integer b){
+        return a - b;
+            }
+            Integer  dividir (Integer a, Integer b){
+                return a / b;
+                    }
+
+}
